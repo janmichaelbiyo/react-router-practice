95c42e10aac8562678ebb5cbc21e092c717a5813
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-01-24 20:22:46 -0600

    this is the first parts of the routes

---
+++ src/components/About/About.jsx
@@ -0,0 +1,9 @@
+function About () {
+    return (
+
+    <p> this is a list of stuff </p>
+    );
+}
+
+
+export default About;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,32 @@
 import axios from 'axios';
+import { HashRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 import StudentForm from '../StudentForm/StudentForm.jsx';
 import StudentList from '../StudentList/StudentList.jsx';
+import About from '../About/About.jsx';
+
 
 function App() {
     return (
         <div className="App">
+            <HashRouter>
             <header className="App-header">
             <h1 className="App-title">GitHub Student List</h1>
             </header>
             <br/>
+            <Route path="/">
             <StudentForm />
+            </Route>
 
+            <Route path="/about">
+            <About />
+            </Route>
+
+            <Route path="/allStudents">
             <p>Student list:</p>
             <StudentList />
+            </Route>
+            </HashRouter>
         </div>
     );
 }
